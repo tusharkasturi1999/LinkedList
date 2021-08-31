8ced730cfda05c0be2adc99bcb9dbc0a7728ad88
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-08-31 23:32:23 +0530

    Added: insertAtStart method to insert elements to the start of the list

--- LinkedList.java
+++ LinkedList.java
@@ -11,7 +11,8 @@ public class LinkedList<T> {
 	Node<T> head;
 
 	/**
-	 * This method is the insert method newNode is created
+	 * This method is the insert method newNode is created Temporary node temp is
+	 * used to traverse
 	 */
 	public void insert(T data) {
 		Node<T> newNode = new Node<>(data);
@@ -29,17 +30,31 @@ public class LinkedList<T> {
 		}
 	}
 
+	public void insertAtStart(T data) {
+		Node<T> newNode = new Node<>(data);
+		newNode.data = data;
+		newNode.next = null;
+
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node<T> temp = head;
+			head = newNode;
+			newNode.next = temp;
+
+		}
+
+	}
+
 	/**
-	 * 
-	 * This method is to print linkedlist temporary node temp is used to traverse
-	 * 
+	 * This method is to print linkedlist Temporary node temp is used to traverse
 	 */
 	public void show() {
 
 		Node<T> temp = head;
 
 		while (temp.next != null) {
-			System.out.println(temp.data);
+			System.out.print(temp.data + "->");
 			temp = temp.next;
 		}
 		System.out.println(temp.data);
